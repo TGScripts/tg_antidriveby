5562c0744e72bd5e517fedf8132cd01ddb87f261
LetsTiger <diezet4159@gmail.com>
2023-03-10 17:48:54 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,4 +2,4 @@ Config = {}
 
 Config.MaxSpeed             =   40              -- in km/h
 Config.CanDriverDriveby     =   false           -- Darf der Fahrer des Fahrzeugs auch schießen?
-Config.Command              =   "driveby"       -- Command der ausgeführt werden muss um schießen zu können
+Config.Command              =   'driveby'       -- Command der ausgeführt werden muss um schießen zu können
